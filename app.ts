607ca940ba304e872b0fdd315942aa0c3fcd43c0
Lilach E <lilachelayza@gmail.com>
2023-11-20 18:28:16 +0200

    first commit

---
+++ app.ts
@@ -0,0 +1,24 @@
+type Action = { action: string; startTime: Date; endTime: Date };
+const actions = [
+  {
+    action: "Study",
+    startTime: new Date(),
+    endTime: new Date(),
+  },
+  {
+    action: "Exercise",
+    startTime: new Date(),
+    endTime: new Date(),
+  },
+  {
+    action: "Read a book",
+    startTime: new Date(),
+    endTime: new Date(),
+  },
+  {
+    action: "Meditate",
+    startTime: new Date(),
+    endTime: new Date(),
+  },
+  
+] as Action[];
---
+++ dist/app.js
@@ -0,0 +1,22 @@
+const actions = [
+    {
+        action: "Study",
+        startTime: new Date(),
+        endTime: new Date(),
+    },
+    {
+        action: "Exercise",
+        startTime: new Date(),
+        endTime: new Date(),
+    },
+    {
+        action: "Read a book",
+        startTime: new Date(),
+        endTime: new Date(),
+    },
+    {
+        action: "Meditate",
+        startTime: new Date(),
+        endTime: new Date(),
+    },
+];
