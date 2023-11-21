0f5a76a37d812b5dd4c3c3fa0dcbb30a3561e605
Lilach E <lilachelayza@gmail.com>
2023-11-21 08:45:31 +0200

    activate log action form

--- app.ts
+++ app.ts
@@ -28,13 +28,32 @@ const actionHistory: ActionLog[] = [
   },
 ];
 
-const actionSelectionElement = document.querySelector("#actionSelection") as HTMLElement;
+const actionSelectionElement = document.querySelector(
+  "#actionSelection"
+) as HTMLElement;
 updateActionSelection(actions, actionSelectionElement);
 
-const actionHistoryElement = document.querySelector(".actionHistory") as HTMLElement;
+const actionHistoryElement = document.querySelector(
+  ".actionHistory"
+) as HTMLElement;
 if (actionHistoryElement) {
   showActionHistory(actionHistory, actionHistoryElement);
 }
+
+document.forms.namedItem("addActionForm")?.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const formData = new FormData(e.target as HTMLFormElement);
+
+  const actionObject: ActionLog = {
+    name: parseData(formData.get("actionSelection"), "action"),
+    startTime: parseData(formData.get("startTime"), "startTime"),
+    endTime: parseData(formData.get("endTime"), "endTime"),
+  };
+
+  logAction(actionObject, actionHistory);
+  showActionHistory(actionHistory, actionHistoryElement);
+});
 
 function showActionHistory(actionHistory: ActionLog[], container: HTMLElement) {
   if (actionHistory.length === 0) {
@@ -49,17 +68,42 @@ function showActionHistory(actionHistory: ActionLog[], container: HTMLElement) {
 function renderActionHistory(action: ActionLog) {
   return `
   <p>Action: ${action.name}</p>
-  <p>Start-Time: ${action.startTime.toLocaleString()}</p>
-  <p>End-Time: ${action.endTime.toLocaleString()}</p>
+  <p>Start-Time: ${formatTime(action.startTime)}</p>
+  <p>End-Time: ${formatTime(action.endTime)}</p>
   `;
 }
 
-function updateActionSelection(actionsArray: typeof actions, container: HTMLElement) {
-    container.innerHTML = `${actionsArray.map(renderActionSelection).join("\n")}`
+function updateActionSelection(
+  actionsArray: typeof actions,
+  container: HTMLElement
+) {
+  container.innerHTML = `${actionsArray.map(renderActionSelection).join("\n")}`;
 }
 
 function renderActionSelection(action: Action) {
-    return `
+  return `
     <option>${action}</option>
-    `
+    `;
+}
+
+function parseData(input: any | null, key: string) {
+  if (input == null) {
+    throw new Error(`${key} can't be null!`);
+  } else {
+    return input;
+  }
+}
+
+function logAction(actionLog: ActionLog, actionHistory: ActionLog[]) {
+  actionHistory.push(actionLog);
+}
+
+function formatTime(time: Date) {
+  return new Date(time).toLocaleString("en-GB", {
+    day: "2-digit",
+    month: "2-digit",
+    year: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+  });
 }
--- dist/app.js
+++ dist/app.js
@@ -27,6 +27,17 @@ const actionHistoryElement = document.querySelector(".actionHistory");
 if (actionHistoryElement) {
     showActionHistory(actionHistory, actionHistoryElement);
 }
+document.forms.namedItem("addActionForm")?.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const actionObject = {
+        name: parseData(formData.get("actionSelection"), "action"),
+        startTime: parseData(formData.get("startTime"), "startTime"),
+        endTime: parseData(formData.get("endTime"), "endTime"),
+    };
+    logAction(actionObject, actionHistory);
+    showActionHistory(actionHistory, actionHistoryElement);
+});
 function showActionHistory(actionHistory, container) {
     if (actionHistory.length === 0) {
         container.innerHTML = `<p>no actions yet!</p>`;
@@ -40,8 +51,8 @@ function showActionHistory(actionHistory, container) {
 function renderActionHistory(action) {
     return `
   <p>Action: ${action.name}</p>
-  <p>Start-Time: ${action.startTime.toLocaleString()}</p>
-  <p>End-Time: ${action.endTime.toLocaleString()}</p>
+  <p>Start-Time: ${formatTime(action.startTime)}</p>
+  <p>End-Time: ${formatTime(action.endTime)}</p>
   `;
 }
 function updateActionSelection(actionsArray, container) {
@@ -51,4 +62,24 @@ function renderActionSelection(action) {
     return `
     <option>${action}</option>
     `;
+}
+function parseData(input, key) {
+    if (input == null) {
+        throw new Error(`${key} can't be null!`);
+    }
+    else {
+        return input;
+    }
+}
+function logAction(actionLog, actionHistory) {
+    actionHistory.push(actionLog);
+}
+function formatTime(time) {
+    return new Date(time).toLocaleString("en-GB", {
+        day: "2-digit",
+        month: "2-digit",
+        year: "numeric",
+        hour: "2-digit",
+        minute: "2-digit",
+    });
 }
